6c3a0ca18baecda5b038c9c5f61a31f005d0f3c3
Maarten <maartenkampmeijer@hotmail.com>
2018-01-18 08:45:46 +0100

    Add files via upload

--- FRO/animatie/js/script.js
+++ FRO/animatie/js/script.js
@@ -3,8 +3,12 @@ let blok = document.querySelectorAll(".blok");
 
 const schuiven = () => {
   blok[0].classList.toggle('blok--verplaatst');
-  blok[1].classList.toggle('blok--verplaatst');
-  blok[2].classList.toggle('blok--verplaatst');
+  setTimeout(() => {
+    blok[1].classList.toggle('blok--verplaatst');
+  },200 );
+  setTimeout(() => {
+    blok[2].classList.toggle('blok--verplaatst');
+  },400 );
 }
 
 button.addEventListener('click', schuiven);
